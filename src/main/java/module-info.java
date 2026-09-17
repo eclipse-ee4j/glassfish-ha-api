ee6d1d36fc783bacff40f52512e7891d458a2ba4
David Matějček <david.matejcek@omnifish.ee>
2026-09-17 18:51:11 +0200

    Fixed HK2 requirement in module-info, as it is just an optional dependency.
    Signed-off-by: David Matějček <david.matejcek@omnifish.ee>

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Contributors to the Eclipse Foundation.
+ * Copyright (c) 2023, 2026 Contributors to the Eclipse Foundation.
  * Copyright (c) 2021 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
@@ -17,7 +17,8 @@
 
 module org.glassfish.ha.api {
     requires java.logging;
-    requires org.glassfish.hk2.api;
+    // HK2 is optional
+    requires static org.glassfish.hk2.api;
 
     exports org.glassfish.ha.store.annotations;
     exports org.glassfish.ha.store.api;
