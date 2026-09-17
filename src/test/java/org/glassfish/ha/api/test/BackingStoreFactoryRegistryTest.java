9844934bce77e6fa0e16be5a0d9d88e6bbd3bf7c
David Matějček <david.matejcek@omnifish.ee>
2026-09-17 20:24:24 +0200

    JUnit test rewritten
    - old used Junit4 dependency, but impl was Junit3 or older, and incorrect
    - some test methods original returned instead of failing
    - to check the test result it depended on Java assert.
    
    Signed-off-by: David Matějček <david.matejcek@omnifish.ee>

--- src/test/java/org/glassfish/ha/api/test/BackingStoreFactoryRegistryTest.java
+++ src/test/java/org/glassfish/ha/api/test/BackingStoreFactoryRegistryTest.java
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2026 Contributors to the Eclipse Foundation.
  * Copyright (c) 1997, 2018 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
@@ -16,227 +17,127 @@
 
 package org.glassfish.ha.api.test;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-import org.glassfish.ha.store.api.*;
-import org.glassfish.ha.store.impl.NoOpBackingStoreFactory;
-import org.glassfish.ha.store.spi.BackingStoreFactoryRegistry;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.Serializable;
-import java.util.Set;
+
+import org.glassfish.ha.store.api.BackingStore;
+import org.glassfish.ha.store.api.BackingStoreFactory;
+import org.glassfish.ha.store.api.Storeable;
+import org.glassfish.ha.store.impl.NoOpBackingStoreFactory;
+import org.glassfish.ha.store.spi.BackingStoreFactoryRegistry;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 /**
  * Unit test for simple App.
  */
-public class BackingStoreFactoryRegistryTest
-        extends TestCase {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public BackingStoreFactoryRegistryTest(String testName) {
-        super(testName);
-    }
+public class BackingStoreFactoryRegistryTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite(BackingStoreFactoryRegistryTest.class);
-    }
+    @Test
+    public void testBackingStoreLifecycle() throws Exception {
+        BackingStoreFactory backingStoreFactory = BackingStoreFactoryRegistry.getFactoryInstance("noop");
+        assertNotNull(backingStoreFactory, "backingStoreFactory");
+        try (BackingStore<Serializable, Serializable> backingStore = backingStoreFactory.createBackingStore(null)) {
+            assertNotNull(backingStore, "backingStore");
+            backingStore.save("k1", null, true);
+            backingStore.save("k1", new NoopData(), true);
+            backingStore.save("k1", null, false);
+            backingStore.save("k1", null, true);
 
+            backingStore.load(null,null);
+            backingStore.load(null, "6");
+            backingStore.load("k1", null);
+            backingStore.load("k1", "6");
 
-    public void testBackingStoreFactoryRegistry() {
-        boolean result = false;
-        try {
-            BackingStoreFactory nbsf = BackingStoreFactoryRegistry.getFactoryInstance("noop");
-            result = true;
-        } catch (BackingStoreException bsEx) {
+            backingStore.remove(null);
+            backingStore.remove("k1");
 
-        }
+            backingStore.updateTimestamp(null, "6", -1L);
+            backingStore.updateTimestamp("k1", "6", -1L);
 
-        assert(result);
-    }
-
-    public void testBackingStore() {
-        boolean result = false;
-        try {
-            BackingStoreFactory nbsf = BackingStoreFactoryRegistry.getFactoryInstance("noop");
-            BackingStoreConfiguration<String, NoopData> conf = null;
-            BackingStore<String, NoopData> bs = nbsf.createBackingStore(conf);
-
-            result = true;
-        } catch (BackingStoreException bsEx) {
-
+            backingStore.removeExpired();
         }
     }
 
-    public void testBackingStoreSave() {
-        boolean result = false;
-        try {
-            BackingStoreFactory nbsf = BackingStoreFactoryRegistry.getFactoryInstance("noop");
-            BackingStoreConfiguration<String, NoopData> conf = null;
-            BackingStore<String, NoopData> bs = nbsf.createBackingStore(conf);
-
-            bs.save("k1", null, true);
-            bs.save("k1", new NoopData(), true);
-            bs.save("k1", null, false);
-            bs.save("k1", null, true);
-
-            bs.load(null,null);
-            bs.load(null, "6");
-            bs.load("k1", null);
-            bs.load("k1", "6");
-
-
-            bs.remove(null);
-            bs.remove("k1");
-
-
-            bs.updateTimestamp(null, "6", -1L);
-            bs.updateTimestamp("k1", "6", -1L);
-
-
-            bs.removeExpired();
-
-            result = true;
-        } catch (BackingStoreException bsEx) {
-
+    @Test
+    public void testBackingStoreUpdateTimestamp() throws Exception {
+        BackingStoreFactory backingStoreFactory = BackingStoreFactoryRegistry.getFactoryInstance("noop");
+        try (BackingStore<Serializable, Serializable> backingStore = backingStoreFactory.createBackingStore(null)) {
+            assertNotNull(backingStore, "backingStore");
+            assertNull(backingStore.updateTimestamp(null, "3", -1L));
+            assertNull(backingStore.updateTimestamp("k1", "2", 0L));
         }
-
-        assert(result);
     }
 
-    public void testBackingStoreLoad() {
-        boolean result = false;
-        try {
-            BackingStoreFactory nbsf = BackingStoreFactoryRegistry.getFactoryInstance("noop");
-            BackingStoreConfiguration<String, NoopData> conf = null;
-            BackingStore<String, NoopData> bs = nbsf.createBackingStore(conf);
-
-            bs.load(null,null);
-            bs.load(null, "6");
-            bs.load("k1", null);
-            bs.load("k1", "6");
-
-            result = true;
-        } catch (BackingStoreException bsEx) {
-
+    @Test
+    public void testBackingStoreRemoveExpired() throws Exception {
+        BackingStoreFactory backingStoreFactory = BackingStoreFactoryRegistry.getFactoryInstance("noop");
+        try (BackingStore<Serializable, Serializable> backingStore = backingStoreFactory.createBackingStore(null)) {
+            assertEquals(0, backingStore.removeExpired());
         }
-
-        assert(result);
-    }
-    public void testBackingStoreRemove() {
-        boolean result = false;
-        try {
-            BackingStoreFactory nbsf = BackingStoreFactoryRegistry.getFactoryInstance("noop");
-            BackingStoreConfiguration<String, NoopData> conf = null;
-            BackingStore<String, NoopData> bs = nbsf.createBackingStore(conf);
-
-            bs.remove(null);
-            bs.remove("k1");
-
-            result = true;
-        } catch (BackingStoreException bsEx) {
-
-        }
-
-        assert(result);
-    }
-    public void testBackingStoreUpdateTimestamp() {
-        boolean result = false;
-        try {
-            BackingStoreFactory nbsf = BackingStoreFactoryRegistry.getFactoryInstance("noop");
-            BackingStoreConfiguration<String, NoopData> conf = null;
-            BackingStore<String, NoopData> bs = nbsf.createBackingStore(conf);
-
-            bs.updateTimestamp(null, "3", -1L);
-            bs.updateTimestamp("k1", "2", 0L);
-
-            result = true;
-        } catch (BackingStoreException bsEx) {
-
-        }
-
-        assert(result);
-    }
-    public void testBackingStoreRemoveExpired() {
-        boolean result = false;
-        try {
-            BackingStoreFactory nbsf = BackingStoreFactoryRegistry.getFactoryInstance("noop");
-            BackingStoreConfiguration<String, NoopData> conf = null;
-            BackingStore<String, NoopData> bs = nbsf.createBackingStore(conf);
-
-            bs.removeExpired();
-
-            result = true;
-        } catch (BackingStoreException bsEx) {
-
-        }
-
-        assert(result);
     }
 
+    @Test
     public void testBackingStoreFactoryRegistryGetRegistered() {
-        boolean result = false;
-        BackingStoreFactoryRegistry.register("foo", new NoOpBackingStoreFactory());
-        Set<String> types = BackingStoreFactoryRegistry.getRegisteredTypes();
-        if (types.size() != 2) {
-            result = false;
-            return;
-        }
-        for (String type : types) {
-            System.out.println("Registered type:" + type);
-        }
+        assertNull(BackingStoreFactoryRegistry.register("foo", new NoOpBackingStoreFactory()));
+        assertThat(BackingStoreFactoryRegistry.getRegisteredTypes(), contains("noop", "memory", "file", "foo"));
         BackingStoreFactoryRegistry.unregister("foo");
-        types = BackingStoreFactoryRegistry.getRegisteredTypes();
-        if (types.size() != 1) {
-            result = false;
-            return;
-        }
-        result = true;
+        assertThat(BackingStoreFactoryRegistry.getRegisteredTypes(), contains("noop", "memory", "file"));
     }
 
-    private static final class NoopData
-        implements Storeable {
+    private static final class NoopData implements Storeable {
 
+        private static final long serialVersionUID = 2714437246016654909L;
+
+        @Override
         public long _storeable_getVersion() {
             return 0;
         }
 
+        @Override
         public void _storeable_setVersion(long version) {
         }
 
+        @Override
         public long _storeable_getLastAccessTime() {
             return 0;
         }
 
+        @Override
         public void _storeable_setLastAccessTime(long version) {
         }
 
+        @Override
         public long _storeable_getMaxIdleTime() {
             return 0;
         }
 
+        @Override
         public void _storeable_setMaxIdleTime(long version) {
         }
 
+        @Override
         public String[] _storeable_getAttributeNames() {
             return new String[0];
         }
 
+        @Override
         public boolean[] _storeable_getDirtyStatus() {
             return new boolean[0];
         }
 
+        @Override
         public void _storeable_writeState(OutputStream os) throws IOException {
         }
 
+        @Override
         public void _storeable_readState(InputStream is) throws IOException {
         }
     }
